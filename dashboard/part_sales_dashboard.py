57e4e701e7ab0d76b69296b68ecab085b6e94b9a
sahrulhino <sahrul.mustakim@hino.co.id>
2025-08-10 16:50:26 +0700

    Update Dashboard Python Scheduler

---
+++ dashboard/part_sales_dashboard.py
@@ -0,0 +1,182 @@
+print ("Start Copying")
+import pyodbc
+import time
+import datetime
+import calendar
+conn = pyodbc.connect('DSN=MDS17PRODDSN;UID=crm;PWD=password')
+cursor = conn.cursor()
+y=0;
+z='';
+x=0;
+yeard = time.strftime("%Y")
+today = datetime.date.today()
+one_day = datetime.timedelta(days=1)
+yesterday = today - one_day
+YY = str(today.year)
+MM=str(today.month)
+DD = str(today.day)
+
+print(YY)
+print(MM)
+
+#todaydate =  yeard+monthd+dayd
+
+cursor.execute("""SELECT 
+REFPE1 ||' ' ||REF#E1 as DO_NO,
+CORDE0 AS PO_NO,
+INYYE1||'/'||INMME1||'/'||INDDE1 as INV_DATE,
+'-' as INV_NO ,
+'-' as Tax_No   ,
+ITMSE1 as PART_NO,
+SUPQE1 as QTY,
+'PC' as UOM,
+WSLEE1 AS PRICELIST,
+DECIMAL(100-(((INVVE1/SUPQE1)/WSLEE1)*100),18,0) AS  DISC,
+DECIMAL(INVVE1/SUPQE1,18,0) AS  AFTER,
+'IDR' as WAERS,
+INVVE1 AS GROSS_AMT,
+REFPE1||REF#E1  as Picking_List,
+INYYE0||'/'||INMME0||'/'||INDDE0 as DELIVERYDATE,
+ODYYE0||'/'||ODMME0||'/'||ODDDE0 AS ORDERDC,
+TCMPE0 AS EXPEDITION ,
+USREE0||' - ',
+CUSTA0 as DEALER_NAME,
+ORPE1 as ORG_PL_PREF,
+OR#E1 as ORG_PL_NO,
+INVPE1 as INV_NO_BY_DO_PREF,
+INV#E1 as INV_NO_BY_DO,
+PRNTI4 as PART_MASKING,
+DESCI4 as PART_NAME,
+SLSTE0 as SALES_TYPE,
+TAXBE1 AS TAX,
+DIVE1 AS DIV,
+ICSTE1 as COST_INVOICE,
+CAT1I4 as CAT_ONE
+
+
+FROM HMI17P001.OEFL0100   T01 LEFT JOIN 
+HMI17P001.INFP04 T02   ON
+
+T01.ITMSE1        =       T02.ITEMI4   AND 
+T01.COMPE1        =       T02.COMPI4    LEFT JOIN 
+
+HMI17P001.ARFL0000 T03 ON
+
+T01.COMPE1        =       T03.COMPA0  AND
+T01.CUSTE1        =       T03.CUSTA0  LEFT JOIN 
+ 
+HMI17P001.OEFL0000 T04 ON
+
+T01.COMPE1        =       T04.COMPE0  AND 
+T01.REFPE1        =       T04.REFPE0 AND
+T01.REF#E1        =       T04.REF#E0 
+
+WHERE    COMPE1            =     '001' 
+ 
+
+AND 	INYYE1				="""+str(YY)+"""
+AND 	INMME1				>=1
+
+
+
+
+AND    PRDCE0            =     '40'               
+ """)
+ #AND 	INMME1				="""+str(MM)+"""
+ #AND   	 GVDRGS        	<>     ''    
+ #AND   	 TTYPGS           =     '2' 
+#AND 	 LEFT(CUSTA0,1) LIKE '%P%'
+#AND    T01.REFPA2        <>     'C' 
+
+ #AND    (INYYE1*10000)+(INMME1*100)+(INDDE1) between 20150101 and 20150731
+ #AND    (INYYE1)+(INMME1)+(INDDE1) between """+str(todaydate)+""" and """+str(todaydate)+"""
+ # AND INYYE1="+str(YY1)+" and INMME1="+str(MM1)+" and INDDE1="+str(DD1)+"
+ # (CUSTA0 IN ('PJARM1'))
+ #AND 	(INYYE1*10000)				= """+str(yeard)+"""
+ #AND 	(INMME1)		= """+str(monthd)+"""
+#AND 	INDDE1						= """+str(dayd)+"""
+
+data = cursor.fetchall()
+y	 = len(data)
+	
+testArr = range(y)
+dataItem = range(30)
+
+for row in data:
+	testArr[x] = range(30)
+	for t in range(0,30):
+		testArr[x][t] = row[t]
+	x=x+1
+
+cursor.close()
+conn.close()
+
+
+#------------------------------------------insert to postgres------------------------------------------
+
+
+	
+
+import mysql.connector
+from mysql.connector import errorcode
+from difflib import SequenceMatcher
+import sys
+
+
+
+try:
+   conn2 	= mysql.connector.connect(host='10.17.51.35',user='mysqlwb',password='mysqlwb',database='hino_bi_db')
+except:
+    print ("I am unable to connect to the database hino_bi_db.")
+	
+cur = conn2.cursor()
+
+
+#cur.execute("delete from bi_part_sales where year(inv_date)="+(YY)+" and  month(inv_date)="+(MM)+"  and do_no<>'-' ");
+cur.execute("delete from bi_part_sales where year(inv_date)="+(YY)+" and  month(inv_date)>=1  and do_no<>'-' ");
+conn2.commit()
+# month(inv_date)="+(MM)+" and
+okchars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_-+=\/,.<> '
+
+for z in range(0,y):
+	for index in range(0,30):
+	
+		dataItem[index] = ''.join(e for e in str(testArr[z][index]).replace("'", " ") if e in okchars)
+		if( dataItem[index] == 'None' ):
+			dataItem[index] ='0';
+		
+	
+	
+	
+	try:
+		cur.execute("insert into bi_part_sales(id_invoice,do_no,po_no,inv_date,inv_summary,seri_pajak,part_no,qty,uom,pricelist,discount,after_discount,waers,gross,picking_list,delivery_date_hmsi,dc_order_date,expedisi,keyedby,dealer_name,original_pl_pref,original_pl_no,inv_by_do_pref,inv_by_do,part_masking,part_name,sales_type, tax,division,cost,item_category_one) VALUES('','"+dataItem[0]+"','""','"+dataItem[2]+"','"+dataItem[3]+"','"+dataItem[4]+"','"+dataItem[5]+"',"+dataItem[6]+",'"+dataItem[7]+"',"+dataItem[8]+","+dataItem[9]+","+dataItem[10]+",'"+dataItem[11]+"',"+dataItem[12]+",'"+dataItem[13]+"','"+dataItem[14]+"','"+dataItem[15]+"','"+dataItem[16]+"','"+dataItem[17]+"','"+dataItem[18]+"','"+dataItem[19]+"','"+dataItem[20]+"','"+dataItem[21]+"','"+dataItem[22]+"','"+dataItem[23]+"','"+dataItem[24]+"','"+dataItem[25]+"','"+dataItem[26]+"','"+dataItem[27]+"','"+dataItem[28]+"','"+dataItem[29]+"')")
+	
+		
+		conn2.commit()
+		print ("sukses")
+	except:
+		print ("failed")
+		print ("insert into bi_part_sales(id_invoice,do_no,po_no,inv_date,inv_summary,seri_pajak,part_no,qty,uom,pricelist,discount,after_discount,waers,gross,picking_list,delivery_date_hmsi,dc_order_date,expedisi,keyedby,dealer_name,original_pl_pref,original_pl_no,inv_by_do_pref,inv_by_do,part_masking,part_name,sales_type,tax,division) VALUES('','"+dataItem[0]+"','"+dataItem[1]+' '",'"+dataItem[2]+"','"+dataItem[3]+"','"+dataItem[4]+"','"+dataItem[5]+"',"+dataItem[6]+",'"+dataItem[7]+"',"+dataItem[8]+","+dataItem[9]+","+dataItem[10]+",'"+dataItem[11]+"',"+dataItem[12]+",'"+dataItem[13]+"','"+dataItem[14]+"','"+dataItem[15]+"','"+dataItem[16]+"','"+dataItem[17]+"','"+dataItem[18]+"','"+dataItem[19]+"','"+dataItem[20]+"','"+dataItem[21]+"','"+dataItem[22]+"','"+dataItem[23]+"','"+dataItem[24]+"','"+dataItem[25]+"','"+dataItem[26]+"','"+dataItem[27]+"')")
+		break;
+print ("Stop Copying..")
+
+try:
+   conn3 	= mysql.connector.connect(host='10.17.51.35',user='mysqlwb',password='mysqlwb',database='hino_bi_db')
+except:
+    print ("I am unable to connect to the database hino_bi_db.")
+	
+cur3 = conn3.cursor()
+
+query = """
+    delete from bi_lastupdate where bi_report = 'bi_part_sales';
+	insert into bi_lastupdate select 'bi_part_sales' as bi_report, now() as last_update;
+"""
+
+for result in cur3.execute(query, multi=True):
+    if result.with_rows:
+        print(result.fetchall())
+
+conn3.commit()
+
+
+
---
+++ dashboard/vehicle_wholesales_live 1.py
@@ -0,0 +1,164 @@
+import pyodbc
+import datetime
+import time
+import calendar
+conn = pyodbc.connect('DSN=MDS17PRODDSN;UID=CRM;PWD=PASSWORD')
+cursor = conn.cursor()
+y=0;
+today = datetime.date.today()
+one_day = datetime.timedelta(days=2)
+yesterday = today - one_day
+dodate = str(yesterday)
+YY = str(today.year)
+MM=str(today.month)
+DD = str(today.day)
+
+#YY = '2025'
+#MM='2'
+
+YY1 = str(yesterday.year)
+MM1 = str(yesterday.month)
+DD1 = str(yesterday.day)
+
+todaydate = str(today.year) +  str(today.month) + str(today.day)
+
+print('Year:', YY)
+print('Mon :', MM1)
+print('Day :', DD1)
+cursor.execute("SELECT  count(*)as total FROM HMI17P001.INFL0700 T01 LEFT JOIN HMI17P001.ARFL0000 T02 ON T01.DLR#I7=T02.CUSTA0  LEFT JOIN HMI17P001.INFL3700 T03 ON T01.VIN#I7=T03.VIN#J1  LEFT JOIN IDS2101D.ZZFL3400 T04 ON T01.VIN#I7= T04.VIN#34 LEFT JOIN HMI17P001.INFL5200 T05 ON T01.VIN#I7=T05.VIN#JF WHERE T01.COMPI7 ='001' AND (T05.FROMJF= '1' AND T05.TOJF ='5') AND T01.DOYYI7 ="+str(YY)+" AND T01.DOMMI7 ="+str(MM)+"")
+#AND T05.MVYYJF="+str(YY1)+" and T05.MVMMJF="+str(MM1)+" and T05.MVDDJF="+str(DD1)+"
+
+for row in cursor:
+	y=row[0]
+	
+testArr = range(y)
+
+
+print(y)
+
+cursor.execute("SELECT T01.VIN#I7,T03.ITEMJ1,T01.MODLI7,T01.DLR#I7,T01.VIN#I7,T01.ENG#I7,T01.COLRI7,T01.DODDI7,T01.DOMMI7,T01.DOYYI7,T01.REF2I7,T01.REFPI7,T01.REF#I7,T02.NAMEA0,T05.FROMJF,T05.TOJF,T05.MVYYJF, T05.MVMMJF,T05.MVDDJF,T01.ETAYI7,T01.ETAMI7,T01.ETADI7,T01.INVPI7,T01.INV#I7 FROM HMI17P001.INFL0700 T01 LEFT JOIN HMI17P001.ARFL0000 T02 ON T01.DLR#I7=T02.CUSTA0  LEFT JOIN HMI17P001.INFL3700 T03 ON T01.VIN#I7=T03.VIN#J1  LEFT JOIN IDS2101D.ZZFL3400 T04 ON T01.VIN#I7= T04.VIN#34 LEFT JOIN HMI17P001.INFL5200 T05 ON T01.VIN#I7=T05.VIN#JF WHERE T01.COMPI7 ='001' AND (T05.FROMJF= '1' AND T05.TOJF ='5') AND T01.DOYYI7 ="+str(YY)+" AND T01.DOMMI7 ="+str(MM)+"")
+
+# AND T01.DOYYI7 ="+str(YY)+" AND T01.DOMMI7 ="+str(MM)+" 
+#OR (T05.FROMJF= '5' AND T05.TOJF ='1') OR (T05.FROMJF= '8' AND T05.TOJF ='1') 
+x=0
+for row in cursor:
+#print row;
+	testArr[x] = range(24)
+	testArr[x][0] = row[0]
+	testArr[x][1] = row[1]
+	testArr[x][2] = row[2]
+	testArr[x][3] = row[3]
+	testArr[x][4] = row[4]
+	testArr[x][5] = row[5]
+	testArr[x][6] = row[6]
+	testArr[x][7] = row[7]
+	testArr[x][8] = row[8]
+	testArr[x][9] = row[9]
+	testArr[x][10] = row[10]
+	testArr[x][11] = row[11]
+	testArr[x][12] = row[12]
+	testArr[x][13] = row[13]
+	testArr[x][14] = row[14]
+	testArr[x][15] = row[15]
+	testArr[x][16] = row[16]
+	testArr[x][17] = row[17]
+	testArr[x][18] = row[18]
+	testArr[x][19] = row[19]
+	testArr[x][20] = row[20]
+	testArr[x][21] = row[21]
+	testArr[x][22] = row[22]
+	testArr[x][23] = row[23]
+	#testArr[x][24] = row[24]
+	#testArr[x][25] = row[25]
+	
+	
+	x=x+1
+	
+	
+cursor.close()
+conn.close()
+#------------------------------------------insert to postgres------------------------------------------
+
+
+	
+
+import mysql.connector
+from mysql.connector import errorcode
+from difflib import SequenceMatcher
+import sys
+
+try:
+	conn2 	= mysql.connector.connect(host='10.17.51.35',user='mysqlwb',password='mysqlwb',database='hino_bi_db')
+except:
+	print("I am unable to connect to the database hino_bi_db.")
+	
+cur = conn2.cursor()
+
+cur.execute("delete from bi_vehicle_wholesales_dealer where year(inv_date)="+(YY)+" and  month(inv_date)="+str(MM)+"  and engine<>''");
+conn2.commit()
+
+okchars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_-+=\/,.<> '
+
+for z in range(0,y):
+	dataItem1 = ''.join(e for e in str(testArr[z][0]).replace("'", " ") if e in okchars)
+	dataItem2 = ''.join(e for e in str(testArr[z][1]).replace("'", " ") if e in okchars)
+	dataItem3 = ''.join(e for e in str(testArr[z][2]).replace("'", " ") if e in okchars)
+	dataItem4 = ''.join(e for e in str(testArr[z][3]).replace("'", " ") if e in okchars)
+	dataItem5 = ''.join(e for e in str(testArr[z][4]).replace("'", " ") if e in okchars)
+	dataItem6 = ''.join(e for e in str(testArr[z][5]).replace("'", " ") if e in okchars)
+	dataItem7 = ''.join(e for e in str(testArr[z][6]).replace("'", " ") if e in okchars)
+	dataItem8 = ''.join(e for e in str(testArr[z][7]).replace("'", " ") if e in okchars)
+	dataItem9 = ''.join(e for e in str(testArr[z][8]).replace("'", " ") if e in okchars)
+	dataItem10 = ''.join(e for e in str(testArr[z][9]).replace("'", " ") if e in okchars)
+	dataItem11 = ''.join(e for e in str(testArr[z][10]).replace("'", " ") if e in okchars)
+	dataItem12 = ''.join(e for e in str(testArr[z][11]).replace("'", " ") if e in okchars)
+	dataItem13 = ''.join(e for e in str(testArr[z][12]).replace("'", " ") if e in okchars)
+	dataItem14 = ''.join(e for e in str(testArr[z][13]).replace("'", " ") if e in okchars)
+	dataItem15 = ''.join(e for e in str(testArr[z][14]).replace("'", " ") if e in okchars)
+	dataItem16 = ''.join(e for e in str(testArr[z][15]).replace("'", " ") if e in okchars)
+	dataItem17 = ''.join(e for e in str(testArr[z][16]).replace("'", " ") if e in okchars)
+	dataItem18 = ''.join(e for e in str(testArr[z][17]).replace("'", " ") if e in okchars)
+	dataItem19 = ''.join(e for e in str(testArr[z][18]).replace("'", " ") if e in okchars)
+	dataItem20 = ''.join(e for e in str(testArr[z][19]).replace("'", " ") if e in okchars)
+	dataItem21 = ''.join(e for e in str(testArr[z][20]).replace("'", " ") if e in okchars)
+	dataItem22 = ''.join(e for e in str(testArr[z][21]).replace("'", " ") if e in okchars)
+	dataItem23 = ''.join(e for e in str(testArr[z][22]).replace("'", " ") if e in okchars)
+	dataItem24 = ''.join(e for e in str(testArr[z][23]).replace("'", " ") if e in okchars)
+	#dataItem25 = ''.join(e for e in str(testArr[z][24]).replace("'", " ") if e in okchars)
+	#dataItem26 = ''.join(e for e in str(testArr[z][25]).replace("'", " ") if e in okchars)
+	
+	
+	
+	
+	try:
+		dodate = str(dataItem10) + "-" + str(dataItem9) + "-" + str(dataItem8)
+		mvndate = str(dataItem17) + "-" + str(dataItem18) + "-" + str(dataItem19)
+		etadate = str(dataItem20) + "-" + str(dataItem21) + "-" + str(dataItem22)
+		invoiceno = str(dataItem23) + " " + str(dataItem24) 
+		chasis=  dataItem5[14:	20]
+		#omnumber = dataItem12 + dataItem13
+		omnumber = str(dataItem12) + str(dataItem13)
+		
+		args = ['','',dataItem1, dataItem2, dataItem3,dataItem4, chasis, dataItem6, dataItem7,dodate, '',dataItem11,omnumber,dataItem14,'','','','','','','',dataItem15,dataItem16,mvndate,etadate,invoiceno]
+		#args = ['','',dataItem1, dataItem2, dataItem3,dataItem4, chasis, dataItem6, dataItem7,dodate, '',dataItem11,omnumber,dataItem14,'','','','','','','',dataItem15,dataItem16,mvndate,etadate,invoiceno,invoicedate]
+
+	
+		result_args = cur.callproc('UPDATE_DEALER_STOCK_STATUS_1_5', args);
+
+		conn2.commit()
+		#print "sukses"
+	except:
+		print(args)
+		#print "INSERT INTO item_parts_copy (COMPI4,ITEMI4,DESCI4,RETLI4,WSLEI4,TWSLI4,DWSLI4,DISCI4,COLRI5,WHSI5,STTEI5,ACTVI5,PRDCI5,DIVI5,CAT1I5,CAT2I5,CAT3I5,CAT4I5,CAT5I5,SOHI5,SOAI5,SOBI5,SOFI5,SOOI5,SORI5,SITI5,WIPI5,ISSI5,SQCI5,SIBI5 ) VALUES ('"+dataItem1+"','"+dataItem2+"','"+dataItem3+"',0,'"+dataItem4+"','"+dataItem5+"',0,0,'','','','','',0,0,'','','','','','"+dataItem6+"','"+dataItem7+"','"+dataItem8+"','"+dataItem9+"','"+dataItem10+"','"+dataItem11+"','"+dataItem12+"','"+dataItem13+"','"+dataItem14+"','"+dataItem15+"')";
+		print("Error on execute UPDATE_DEALER_STOCK_STATUS_1_5 procedure.")
+		break;
+		
+	
+	
+# rows = cur.fetchall()
+# for row in rows:
+    # print "   ", row['item_no']	
+
+
+
+
